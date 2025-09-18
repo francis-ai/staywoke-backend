ff9aa5d7e93d343c6417ae8d82342212ed29e36b
First-App <oladotunfrancisadedayo121@gmail.com>
2025-09-18 18:14:39 +0100

    second commit

--- index.js
+++ index.js
@@ -37,4 +37,8 @@ app.use("/api/products", productRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log("✅ StayWoke backend is running!");
+});
+
